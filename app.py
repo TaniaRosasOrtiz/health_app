7820f08719370ecb52a6f370e2c84d483a67aea6
abigail-gg <abigail.2g@gmail.com>
2021-06-26 13:40:44 -0500

    send email

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, request, render_template 
+from flask import Flask, request, render_template
 import pickle
 import numpy as np
+import smtplib
+from config import key
 
 randomForest = 'rf_model.pkl'
 classifier = pickle.load(open(randomForest, 'rb'))
 
+def send_alert_email(subject, text, mail):
+    senderEmail = 'healthdiabetesapp@gmail.com'
+    message = 'Subject: {}\n\n{}'.format(subject, text)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(senderEmail, key)
+    print("Log: Login success to send email")
+    server.sendmail(senderEmail, mail, message)
+    print("emails have been sent")
+    
+
 # create instance of Flask app
 app = Flask(__name__)
+
 
 
 # create route that renders index.html template
@@ -17,6 +31,7 @@ def home():
 @app.route('/result', methods = ['POST'])
 def result():
     if request.method == 'POST':
+        mail = request.form['email']
         glucose = request.form['glucose']
         weight = float(request.form['weight'])
         height = float(request.form['height'])
@@ -26,8 +41,16 @@ def result():
         
         user_data = np.array([[glucose,bmi, age]])
         new_prediction = classifier.predict(user_data)
+    if new_prediction == 1:
+        text1= 'Hello, {}, we regret to inform you that, according to your information you have a very high risk of having or getting diabetes.'.format(name) 
+        
+    else:
+        text1= 'Hello, {}, we are happy to inform you that according to your information you have a low risk of having or gettin diabetes.'.format(name)
 
-        return render_template('result.html', prediction = new_prediction, nameVariable = name)
+    send_alert_email(subject = "Diabetes Result", text = text1, mail = mail )
+
+    return render_template('result.html', prediction = new_prediction, nameVariable = name)
+
 
 if __name__ == '__main__':
 
