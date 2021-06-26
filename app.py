ad669bd29f6806c24bccb3832cbcc787aabc1525
Daniela Marroquín <daniela.montufar18@gmail.com>
2021-06-26 11:12:06 -0500

    Last update health app

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template ('index.html')
-
+# route for getting prediction results based on form inputs
 @app.route('/result', methods = ['POST'])
 def result():
     if request.method == 'POST':
